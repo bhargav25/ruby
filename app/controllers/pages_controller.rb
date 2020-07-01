64c95aa3474b9da942b4c58bd579c2c59b370069
Bhargav Sridhar <ramsbhargav@gmail.com>
2020-07-01 21:01:53 +0530

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+    def home #html file name same as this method name in views
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  root 'pages#home' #pages is the name of the controller. Create controller from terminal
+end
